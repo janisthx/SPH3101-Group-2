2209d685cd624c3689559e3fda1ecd49f8842697
bernaguette <yeobernadette@hotmail.com>
2025-08-25 13:48:12 +0800

    cleaned household head age data

--- scripts/clean data.R
+++ scripts/clean data.R
@@ -1,18 +1,50 @@
-bdhs = read.csv('data/bdhs.csv')
+bdhs <- read.csv('data/bdhs.csv')
 
-#interested in:
+#pull variables of interest
+
+bdhs_interest <- bdhs[, c("V102", "V150", "V151", "V152")] 
+
+#rename variables
+
+names(bdhs_interest)[names(bdhs_interest) == "V102"] <- "residence"
+names(bdhs_interest)[names(bdhs_interest) == "V150"] <- "relationship"
+names(bdhs_interest)[names(bdhs_interest) == "V151"] <- "sex_head"
+names(bdhs_interest)[names(bdhs_interest) == "V152"] <- "age_head"
+
+#check for NA values
+
+anyNA(bdhs_interest$residence)
+anyNA(bdhs_interest$relationship)
+anyNA(bdhs_interest$sex_head)
+anyNA(bdhs_interest$age_head)
+
+  #no NA values
+
 #V102 type of place of residence (rural/urban)
+
+bdhs_interest$residence <- factor(bdhs_interest$residence, levels = c(1,2), labels = c("Urban", "Rural"))
+summary(bdhs_interest$residence)
+
 #V150 relationship to household head
+
+bdhs_interest$relationship <- factor(bdhs_interest$relationship, levels = c(1,2,3,4,5,6,7,8,9,10,11,12,98), labels = c("Head","Wife","Daughter","Daughter-in-law","Granddaughter", "Mother","Mother-in-law","Sister","Co-spouse","Other relative", "Adopted/foster child","Not related","Don't know"))
+summary(bdhs_interest$relationship)
+
 #V151 sex of household head
+
+bdhs_interest$sex_head <- factor(bdhs_interest$sex_head, levels = c(1,2), labels=c("Male", "Female"))
+summary(bdhs_interest$sex_head)
+
 #V152 age of household head
 
-bdhs$V102 <- factor(bdhs$V102, levels = c(1,2), labels = c("Urban", "Rural"))
-summary(bdhs$V102)
+summary(bdhs_interest$age_head)
+hist(bdhs_interest$age_head,breaks = 100,xlab="Age", main="Histogram of Age of Household Head")
+boxplot(bdhs_interest$age_head,ylab="Age", main="Boxplot of Age of Household Head",col = 'lavender')
 
-bdhs$V150 <- factor(bdhs$V150, levels = c(1,2,3,4,5,6,7,8,9,10,11,12,98), labels = c("Head","Wife","Daughter","Daughter-in-law","Granddaughter", "Mother","Mother-in-law","Sister","Co-spouse","Other relative", "Adopted/foster child","Not related","Don't know"))
-summary(bdhs$V150)
+  #remove outliers
 
-bdhs$V151 <- factor(bdhs$V151, levels = c(1,2), labels=c("Male", "Female"))
-summary(bdhs$V151)
+boxplot.stats(bdhs_interest$age_head)$out
+bdhs_interest$age_head_clean <- bdhs_interest$age_head
+bdhs_interest$age_head_clean[bdhs_interest$age_head_clean %in% boxplot.stats(bdhs_interest$age_head)$out] <- NA
+boxplot(bdhs_interest$age_head_clean,ylab="Age", main="Boxplot of Age of Household Head",col = 'lavender')
 
-summary(bdhs$V152)
