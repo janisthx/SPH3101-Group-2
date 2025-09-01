fffb6d8fb06315daf8da63d9090e0451fa37e08c
bernaguette <yeobernadette@hotmail.com>
2025-09-01 12:19:37 +0800

    added investigation by family type

--- scripts/2_bmi.R
+++ scripts/2_bmi.R
@@ -7,22 +7,32 @@ bdhs_interest$child_bmi <- bdhs_interest$child_weight_clean /
 summary(bdhs_interest$child_bmi[bdhs_interest$residence == "Rural"])
 summary(bdhs_interest$child_bmi[bdhs_interest$residence == "Urban"])
 
-# Rural
-hist(bdhs_interest$child_bmi[bdhs_interest$residence == "Rural"],
-     main = "BMI of Children in Rural Areas",
-     xlab = "BMI",
-     col = "skyblue", border = "white")
-
-# Urban
-hist(bdhs_interest$child_bmi[bdhs_interest$residence == "Urban"],
-     main = "BMI of Children in Urban Areas",
-     xlab = "BMI",
-     col = "salmon", border = "white")
-
+#residence
 boxplot(child_bmi ~ residence, data = bdhs_interest,
         main = "BMI of Children by Residence",
         xlab = "Residence",
         ylab = "BMI",
         col = c("skyblue", "salmon"))
 
+hist(bdhs_interest$child_bmi[bdhs_interest$residence == "Urban"],
+     main = "BMI of Children in Urban Area",
+     xlab = "BMI", col = "skyblue", border = "white")
 
+hist(bdhs_interest$child_bmi[bdhs_interest$residence == "Rural"],
+     main = "BMI of Children in Urban Area",
+     xlab = "BMI", col = "salmon", border = "white")
+
+#family type
+boxplot(child_bmi ~ relationship_trad, data = bdhs_interest,
+        main = "BMI of Children by Family Type",
+        xlab = "Family Type",
+        ylab = "BMI",
+        col = c("lightgreen","orange"))
+
+hist(bdhs_interest$child_bmi[bdhs_interest$relationship_trad == "Traditional"],
+     main = "BMI of Children in Traditional Families",
+     xlab = "BMI", col = "lightgreen", border = "white")
+
+hist(bdhs_interest$child_bmi[bdhs_interest$relationship_trad == "Non-traditional"],
+     main = "BMI of Children in Non-traditional Families",
+     xlab = "BMI", col = "orange", border = "white")
